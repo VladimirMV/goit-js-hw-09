9babbdcf81f388796364126656522359386add51
VladimirMV <33041336+VladimirMV@users.noreply.github.com>
2023-02-01 22:55:46 +0000

    Deploying to gh-pages from @ VladimirMV/goit-js-hw-09@6e92446fc3c96fa73f4f928b590482f357db7fc3 🚀

---
+++ 01-color-switcher.310e580c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");console.log(t),t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,intervalId=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){t.disabled=!1,n.disabled=!0,clearInterval(intervalId)}))}();
+//# sourceMappingURL=01-color-switcher.310e580c.js.map
