c59f85a269a898ce9e9320b62fc27895da6eefd3
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-02-02 00:20:00 +0200

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+// Задание для копилота
+// Write a script that, after pressing the "Start" button, changes the background 
+// color of the <body> once per second to a random value using the inline style. 
+// When you click on the "Stop" button, the background color change should stop.
+// Instead of var use const and let
+//  Keep in mind that you can press the "Start" button an infinite number of times.
+//  Make it so that while the theme change is running, the "Start" button
+//  was not active (disabled).
+
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+console.log(startBtn);
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+});
+// генератор случайного цвета
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
